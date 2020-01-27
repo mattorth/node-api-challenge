1d7a37c7f50118e825fadbea59c919f5a1658b83
mattorth <orth.mj@gmail.com>
2020-01-26 20:00:30 -0800

    projectRouter complete

--- server.js
+++ server.js
@@ -3,32 +3,28 @@ const express = require('express');
 
 // create server
 const server = express();
-const port = process.env.PORT || 5000;
 
 // import middleware
 const helmet = require('helmet');
 const morgan = require('morgan');
+// const cors = require('cors');
 
 // global middleware
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
-server.use(cors);
+// server.use(cors);
 
 // routers
 const actionRouter = require('./routers/actionRouter');
+const projectRouter = require('./routers/projectRouter');
 
 // bind routers
 server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
 
 server.use('/', (req, res) => {
     res.send(`<h1>Node API Challenge</h1>`);
-})
-
-
-// listen
-server.listen(port, () => {
-    console.log(`*** Server listening on port ${port} ***`);
 })
 
 module.exports = server;
